676a500c07f33beff4107c5327c0cf0dd3d375a0
Rishit Shivesh <55372921+rishitshivesh@users.noreply.github.com>
2026-08-30 04:26:28 +0530

    refactor(web): use rest route types

--- apps/web/components/flights/routes/index.tsx
+++ apps/web/components/flights/routes/index.tsx
@@ -1,39 +1,47 @@
-import {Map} from "@/components/ui/map";
-import {FlightRoute} from "@/components/ui/flight";
-import {RoutesResult} from "@/queries/routes/list";
+import { Map } from '@/components/ui/map';
+import { FlightRoute } from '@/components/ui/flight';
+import type { PaginatedResponse, Route } from '@/lib/api-types';
 
-export default function RoutesDemo({routes}: { routes?: RoutesResult['routes'] }) {
-    if (!routes || routes.data?.length === 0) {
-        return <div>No routes available</div>;
-    }
+export default function RoutesDemo({
+  routes,
+}: {
+  routes?: PaginatedResponse<Route>;
+}) {
+  if (!routes || routes.data.length === 0) {
+    return <div>No routes available</div>;
+  }
 
-
-    return (
-        <div className="h-screen w-screen">
-            <Map className="h-full w-full" center={[121.5, 25]} zoom={3}>
-                {routes.data?.map((route, index) => (
-                    <FlightRoute
-                        airportInfo={{
-                            from: {
-                                ...route.departureAirport,
-                                latitude: route.departureCoordinates.latitude,
-                                longitude: route.departureCoordinates.longitude
-                            },
-                            to: {
-                                ...route.arrivalAirport,
-                                latitude: route.arrivalCoordinates.latitude,
-                                longitude: route.arrivalCoordinates.longitude
-                            },
-                        }}
-                        key={route.routeNo + index}
-                        from={[route.departureCoordinates.longitude, route.departureCoordinates.latitude]}
-                        to={[route.arrivalCoordinates.longitude, route.arrivalCoordinates.latitude]}
-                        showAirports
-                        showLabel
-                        //markerContent={<div>{route.arrivalAirport}</div>}
-                    />
-                ))}
-            </Map>
-        </div>
-    );
+  return (
+    <div className="h-screen w-screen">
+      <Map className="h-full w-full" center={[121.5, 25]} zoom={3}>
+        {routes.data.map((route, index) => (
+          <FlightRoute
+            airportInfo={{
+              from: {
+                ...route.departureAirport,
+                latitude: route.departureCoordinates.latitude,
+                longitude: route.departureCoordinates.longitude,
+              },
+              to: {
+                ...route.arrivalAirport,
+                latitude: route.arrivalCoordinates.latitude,
+                longitude: route.arrivalCoordinates.longitude,
+              },
+            }}
+            key={route.routeNo + index}
+            from={[
+              route.departureCoordinates.longitude,
+              route.departureCoordinates.latitude,
+            ]}
+            to={[
+              route.arrivalCoordinates.longitude,
+              route.arrivalCoordinates.latitude,
+            ]}
+            showAirports
+            showLabel
+          />
+        ))}
+      </Map>
+    </div>
+  );
 }
